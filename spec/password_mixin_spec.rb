42d3caa28ab34eb3073a8e4e0a4040abcbd2e77e
Jason <jfrey@redhat.com>
2018-12-13 15:13:41 -0500

    Move PasswordMixin from ManageIQ with specs

---
+++ spec/password_mixin_spec.rb
@@ -0,0 +1,87 @@
+require 'manageiq/password/password_mixin'
+
+RSpec.describe ManageIQ::Password::PasswordMixin do
+  before do
+    @old_key_root = ManageIQ::Password.key_root
+    ManageIQ::Password.key_root = File.join(__dir__, "support")
+  end
+
+  after do
+    ManageIQ::Password.key_root = @old_key_root
+  end
+
+  let(:fake_ar_base) do
+    Class.new do
+      attr_reader :attributes
+
+      def initialize
+        @attributes = {}
+      end
+
+      def read_attribute(attribute)
+        @attributes[attribute]
+      end
+
+      def write_attribute(attribute, value)
+        @attributes[attribute] = value
+      end
+
+      def self.define_column(column)
+        include Module.new do
+          define_method(column) do
+            read_attribute(column)
+          end
+
+          define_method("#{column}=") do |value|
+            write_attribute(column, value)
+          end
+        end
+      end
+
+      def self.class_attribute(attribute)
+        instance_eval(<<~DEF, __FILE__, __LINE__ + 1)
+          def self.#{attribute}
+            @#{attribute}
+          end
+
+          def self.#{attribute}=(value)
+            @#{attribute} = value
+          end
+        DEF
+      end
+    end
+  end
+
+  let(:fake_ar_model) do
+    Class.new(fake_ar_base) do
+      define_column :username
+      define_column :password
+
+      include ManageIQ::Password::PasswordMixin
+      encrypt_column :password
+    end
+  end
+
+  it ".encrypted_columns" do
+    expect(fake_ar_model.encrypted_columns).to match_array(%w[password password_encrypted])
+  end
+
+  it "underlying columns are stored encrypted" do
+    m = fake_ar_model.new
+
+    m.password = "pa$$w0rd"
+    expect(m.attributes[:password]).to be_encrypted
+  end
+
+  it "creates getters and setters" do
+    m = fake_ar_model.new
+
+    m.password = "pa$$w0rd"
+    expect(m.password).to eq("pa$$w0rd")
+    expect(m.password_encrypted).to be_encrypted
+
+    m.password_encrypted = ManageIQ::Password.encrypt("s00persecret")
+    expect(m.password).to eq("s00persecret")
+    expect(m.password_encrypted).to be_encrypted
+  end
+end
